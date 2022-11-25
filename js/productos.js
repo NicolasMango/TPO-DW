7eb21c853ee7475b25365a36572abb9d1565cdcb
Nicolas Mango <nicolas.a.mango@gmail.com>
2022-11-25 18:51:32 -0300

    fix web

---
+++ js/productos.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const productos = [];
+
+productos[0] = [];
+productos[0]['Nombre'] = 'Búsquedas';
+productos[0]['Precio'] = 1600;
+productos[0]['Descripcion'] = 'Contamos con base de datos propias.';
+productos[0]['Imagen'] = '002-upstairs.svg';
+
+productos[1] = [];
+productos[1]['Nombre'] = 'Coaching';
+productos[1]['Precio'] = 2600;
+productos[1]['Descripcion'] = 'Desarrollo Gerencial de su equipo de trabajo.';
+productos[1]['Imagen'] = '003-location.svg';
+
+productos[2] = [];
+productos[2]['Nombre'] = 'Encuestas de Clima';
+productos[2]['Precio'] = 3600;
+productos[2]['Descripcion'] = 'Auditoría de Calidad para mejorar el rendimiento interno.';
+productos[2]['Imagen'] = '004-auction.svg';
+
+productos[3] = [];
+productos[3]['Nombre'] = 'Evaluaciones';
+productos[3]['Precio'] = 4600;
+productos[3]['Descripcion'] = 'Psicotécnicos, Evaluación por Competencias y Evaluación de Potencial.';
+productos[3]['Imagen'] = '005-time is money.svg';
+
+productos[4] = [];
+productos[4]['Nombre'] = 'Procesos de Outplacement';
+productos[4]['Precio'] = 5600;
+productos[4]['Descripcion'] = 'Reinserción Laboral para todos sus colaboradores y personal.';
+productos[4]['Imagen'] = '006-pie chart.svg';
+
+productos[5] = [];
+productos[5]['Nombre'] = 'Capacitaciones';
+productos[5]['Precio'] = 6600;
+productos[5]['Descripcion'] = 'Desarrollamos distintas propuestas a medida, dependiendo de la necesidad.';
+productos[5]['Imagen'] = '008-file.svg';
